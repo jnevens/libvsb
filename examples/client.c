bb08f239e5d62a5c236065c18d4514b8cea1c4d4
Jasper Nevens <jasper.nevens@linux.com>
2016-02-01 22:12:45 +0100

    examples: add possibility to create tcp/ip server and client

--- examples/client.c
+++ examples/client.c
@@ -2,12 +2,81 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
+#include <argp.h>
 
 #include <libevquick.h>
 #include <libvsb/client.h>
 
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+	bool use_tcp;
+	char *unix_sock;
+	char *inet_ip;
+	char *data;
+	uint16_t inet_port;
+};
+
 vsb_client_t *vsb_client = NULL;
+
+/* Default arguments */
+struct arguments arguments = { .use_tcp = false, .inet_port = 12221 };
+
+/* Program documentation. */
+static char doc[] = "vsb client side example.";
+
+/* A description of the arguments we accept. */
+static char args_doc[] = "client [options] UNIX_SOCKET DATA\n" \
+			"client -t [options] DESTINATION_IP PORT DATA";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+		{ "tcp", 't', 0, 0, "Use tcp/ip socket instead of unix socket" },
+		{ 0 }
+	};
+
+/* Parse a single option. */
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	/* Get the input argument from argp_parse, which we
+	 know is a pointer to our arguments structure. */
+	struct arguments *arguments = state->input;
+
+	switch (key) {
+	case 't':
+		arguments->use_tcp = true;
+		break;
+	case ARGP_KEY_ARG:
+		if(arguments->use_tcp) {
+			if (state->arg_num == 0)
+				arguments->inet_ip = arg;
+			else if (state->arg_num == 1)
+				arguments->inet_port = strtoll(arg, NULL, 16);
+			else if (state->arg_num == 2)
+				arguments->data= arg;
+
+		} else {
+			if (state->arg_num == 0)
+				arguments->unix_sock = arg;
+			else if (state->arg_num == 1)
+				arguments->data= arg;
+		}
+		break;
+	case ARGP_KEY_END:
+		if ((arguments->use_tcp && state->arg_num < 3)
+				|| (!arguments->use_tcp && state->arg_num < 2)) {
+			argp_usage(state);
+		}
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 void incoming_data(void *data, size_t len, void *arg)
 {
@@ -40,19 +109,25 @@ int main(int argc, char *argv[])
 {
 	evquick_init();
 
-	if (argc < 3) {
-		printf("usage:%s <pathname> <data>\n", argv[0]);
-		exit(1);
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	if(arguments.use_tcp)
+		vsb_client = vsb_client_init_tcp(arguments.inet_ip, arguments.inet_port, "example-client");
+	else
+		vsb_client = vsb_client_init(arguments.unix_sock, "example-client");
+
+	if(vsb_client == NULL) {
+		fprintf(stderr, "Failed to connect!\n");
+		return -1;
 	}
 
-	vsb_client = vsb_client_init(argv[1], "example-client");
 	int vsb_client_fd = vsb_client_get_fd(vsb_client);
 	vsb_client_register_incoming_data_cb(vsb_client, incoming_data, NULL);
 	evquick_addevent(vsb_client_fd, EVQUICK_EV_READ, handle_incoming_event, NULL, vsb_client);
 	vsb_client_register_disconnect_cb(vsb_client, handle_connection_disconnect, NULL);
 
 	// simulation of data to send
-	evquick_addtimer(1000, EVQUICK_EV_RETRIGGER, generate_data, argv[2]);
+	evquick_addtimer(1000, EVQUICK_EV_RETRIGGER, generate_data, arguments.data);
 
 	evquick_loop();
 	vsb_client_close(vsb_client);
--- examples/server.c
+++ examples/server.c
@@ -7,11 +7,69 @@
 #include <sys/un.h>
 #include <stdio.h>
 #include <errno.h>
+#include <argp.h>
 
 #include <libevquick.h>
 #include <libvsb/server.h>
 
 static evquick_event *server_event = NULL;
+
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+	bool use_tcp;
+	char *unix_sock;
+	uint16_t inet_port;
+};
+
+/* Default arguments */
+struct arguments arguments = { .use_tcp = false, .inet_port = 12221 };
+
+/* Program documentation. */
+static char doc[] = "vsb server side example.";
+
+/* A description of the arguments we accept. */
+static char args_doc[] = "server [options] UNIX_SOCKET\n" \
+			"server -t [options] PORT";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+		{ "tcp", 't', 0, 0, "Use tcp/ip socket instead of unix socket" },
+		{ 0 }
+	};
+
+/* Parse a single option. */
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	/* Get the input argument from argp_parse, which we
+	 know is a pointer to our arguments structure. */
+	struct arguments *arguments = state->input;
+
+	switch (key) {
+	case 't':
+		arguments->use_tcp = true;
+		break;
+	case ARGP_KEY_ARG:
+		if(arguments->use_tcp) {
+			if (state->arg_num == 0)
+				arguments->inet_port = strtoll(arg, NULL, 16);
+		} else {
+			if (state->arg_num == 0)
+				arguments->unix_sock = arg;
+		}
+		break;
+	case ARGP_KEY_END:
+		if (state->arg_num < 1) {
+			argp_usage(state);
+		}
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 void incoming_connection_callback(int fd, short revents, void *arg)
 {
@@ -44,10 +102,23 @@ void new_connection_callback(vsb_conn_t *vsb_conn, void *arg)
 
 int main(int argc, char *argv[])
 {
+	vsb_server_t *vsb_server = NULL;
 	evquick_init();
-	const char *path = "/tmp/vsb.socket";
-	unlink(path);
-	vsb_server_t *vsb_server = vsb_server_init(path);
+
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	if(arguments.use_tcp) {
+		vsb_server = vsb_server_init_tcp(arguments.inet_port);
+	}else {
+		unlink(arguments.unix_sock);
+		vsb_server = vsb_server_init(arguments.unix_sock);
+	}
+
+	if(vsb_server == NULL) {
+		fprintf(stderr, "Failed to open socket!\n");
+		return -1;
+	}
+
 	vsb_server_register_new_connection_cb(vsb_server, new_connection_callback, NULL);
 	fprintf(stderr, "VSB server (fd = %d)\n", vsb_server_get_fd(vsb_server));
 	server_event = evquick_addevent(vsb_server_get_fd(vsb_server), EVQUICK_EV_READ, incoming_vsb_server_event, NULL, vsb_server);
